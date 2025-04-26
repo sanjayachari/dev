065ed39a7d8e0ada4218528ae8b20755ca925b2a
sanjayachari <sanjayacharya992@gmail.com>
2025-04-26 14:03:31 +0530

    LATEST

--- index.js
+++ index.js
@@ -2,3 +2,6 @@ console.log('Hello world1')
  console.log('object')
  console.log('update w')
  console.log('update w')
+
+ const index1 = require('./index1.js');
+ index1.fun();
--- index1.js
+++ index1.js
@@ -1,1 +1,5 @@
 console.log('SANNNNN')
+
+export const fun = () => {
+    console.log('hello LATEST')
+};
